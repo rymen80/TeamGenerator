193cf6cda8398f33b5cd392a71558f3e4c3680cf
Ryan Mendez <rjmendez80@gmail.com>
2020-09-23 13:57:56 -0700

    added engineer class

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+const inquirer = require("inquirer");
+
+class Engineer extends Employee{
+    constructor(name, id, email, gitHubUname) {
+        super(name, email, id);
+        this.gitHub = gitHub;
+    }
+    getGitHubUName() {
+        return this.getGitHub()
+    }
+    getRole() {
+        return "Engineer";
+    }
+    gitUserName() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "gitUsername",
+                message: "Please enter your gitHub user name:"
+            }
+        ]);
+    }
+}
+module.exports = Engineer;

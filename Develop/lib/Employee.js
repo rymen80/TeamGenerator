91d70fe330d85af437ccc92be378c674bf35c046
Ryan Mendez <rjmendez80@gmail.com>
2020-09-23 13:39:14 -0700

    added employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,45 @@
 // TODO: Write code to define and export the Employee class
+const inquirer = require("inquirer");
+
+class Employee{
+    constructor(name,id,email) {
+        this.name = name;
+        this.id = id;
+        this.email =email;
+    }
+    getName() {
+        return this.name
+    }
+    getId() {
+        return this.id
+    }
+    getEmail() {
+        return this.email
+    }
+    getRole() {
+        return "Employee"
+    }
+    questions() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter a name:"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter an ID:"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter an email address:"
+
+            }
+
+        ])
+    }
+
+}
+module.exports = Employee;

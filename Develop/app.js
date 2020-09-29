fc17c49540f6ca84e5f658359a8ac9e4676c8b49
Ryan Mendez <rjmendez80@gmail.com>
2020-09-28 23:20:28 -0700

    working with updated readme

--- Develop/app.js
+++ Develop/app.js
@@ -6,11 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 
-
-
 const employees = [];
-
-
 
 
 const managerQuestions =[
@@ -109,12 +105,7 @@ const addManager = async () => {
     employees.push(manager);
 
         let addTeamMember = answers.addTeamMember;
-        if (addTeamMember === 'intern') {
-            addIntern()
-        }else if( addTeamMember === 'engineer') {
-            addEngineer()
-        }else{
-            fs.writeFileSync(outputPath, render(employees),"utf-8");}
+        checkAddMember(addTeamMember)
 
     })
 }
@@ -125,13 +116,7 @@ const addEngineer = async () => {
         const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGitHub)
         employees.push(engineer);
         let addTeamMember = answers.addTeamMember;
-        if (addTeamMember === 'intern') {
-            addIntern()
-        }else if( addTeamMember === 'manager') {
-            addManager()
-        }else{
-            fs.writeFileSync(outputPath, render(employees),"utf-8");}
-
+        checkAddMember(addTeamMember)
     })
 }
 const addIntern = async () => {
@@ -139,25 +124,32 @@ const addIntern = async () => {
         const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool)
         employees.push(intern)
         let addTeamMember = answers.addTeamMember;
-        if( addTeamMember === 'engineer') {
-            addEngineer()
-        }else if( addTeamMember === 'manager') {
-            addManager()
-        }else{
-            fs.writeFileSync(outputPath, render(employees),"utf-8");}
+       checkAddMember(addTeamMember)
 
     })
 }
+const checkAddMember = newMember => {
+    switch (newMember) {
+        case 'manager':
+            addManager();
+            break;
+        case 'engineer':
+            addEngineer();
+            break;
+        case 'intern':
+            addIntern();
+            break;
+         case 'no':
+             fs.writeFileSync(outputPath, render(employees),"utf-8");
+             break;
+        default:
+            fs.writeFileSync(outputPath, render(employees),"utf-8");
+            break;
+    }
 
+}
 
     addManager()
-
-
-
-        // fs.writeFileSync(outputPath, render(employees),"utf-8");
-
-
-
 
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -166,25 +158,3 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
